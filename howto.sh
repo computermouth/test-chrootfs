a32bdc2f2431fd4f578b22569a437e48ac7b32d1
computermouth <computermouth@crunchbangplusplus.org>
2016-07-19 11:56:55 -0700

    Separate to setup and build

--- howto.sh
+++ howto.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ $(grep "tar \${LIVE_IMAGE_NAME}-\${LIVE_IMAGE_ARCHITECTURE}." /usr/lib/live/build/binary_tar) ]]; then
-	sudo sed -s -i 's%tar ${LIVE_IMAGE_NAME}-${LIVE_IMAGE_ARCHITECTURE}.%tar binary%' /usr/lib/live/build/binary_tar
-fi
-
 lb config\
  --apt-indices none\
  --architectures armhf\
@@ -23,4 +19,3 @@ lb config\
  --bootstrap-qemu-arch armhf\
  --bootstrap-qemu-static /usr/bin/qemu-arm-static
 
-sudo lb build
